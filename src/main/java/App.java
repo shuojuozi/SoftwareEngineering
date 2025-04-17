a9da5816188c0cd48d4c95682ca6b55563238fe8
rzm <1009722799@qq.com>
2025-04-17 19:04:38 +0800

    optimized the structure

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,12 @@ import pojo.Transaction;
 import utils.DeepSeek;
 import utils.JsonUtils;
 
+import java.io.IOException;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        String result = DeepSeek.classifyTransaction("\"4200002635202503247357723190\"");
+        String result = DeepSeek.classifyTransaction("4200002635202503247357723190");
         System.out.println(result);
     }
 }
